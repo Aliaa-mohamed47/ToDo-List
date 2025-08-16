ebb6146e00c025ea0d5c50e22dc309c35dc87df8
Aliaa Mohamed <aliaamohamed472005@gmail.com>
2025-08-16 20:49:26 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPage from "./pages/LandingPage";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+
+
+function App() {
+  return <LandingPage />;
+  // return <SignIn />;
+  // return <SignUp />;
+}
+
+export default App;
