f5054d78d0bd16d28ecbd9054da4ae9f566fdd2f
Aliaa Mohammed <alia_20230357@fci.helwan.edu.eg>
2025-09-05 17:18:19 +0300

    Update WelcomeSection.jsx

--- src/components/HomePage/WelcomeSection.jsx
+++ src/components/HomePage/WelcomeSection.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const WelcomeSection = ({ title, description, buttonText }) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/UpcomingPage");
+    };
+
     return (
         <div className="rounded-menu bg-gray flex flex-col items-center text-center px-6 sm:px-12 md:px-20 lg:px-32">
             <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4 pt-20 lg:pt-48 lg:self-center lg:text-left">
@@ -11,7 +18,10 @@ const WelcomeSection = ({ title, description, buttonText }) => {
                 {description}
             </p>
 
-            <button className="bg-lime text-black h-10 rounded-lg hover:bg-lime-hover transition px-10 sm:px-16 md:px-20 lg:px-[78px] mt-12 whitespace-nowrap mb-8 lg:mb-0">
+            <button
+                onClick={handleClick}
+                className="bg-lime text-black h-10 rounded-lg hover:bg-lime-hover transition px-10 sm:px-16 md:px-20 lg:px-[78px] mt-12 whitespace-nowrap mb-8 lg:mb-0"
+            >
                 {buttonText}
             </button>
         </div>
