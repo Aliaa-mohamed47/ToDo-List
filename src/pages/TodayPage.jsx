d523eb22f6d9067c685dc969322b59c7c7fadc84
Aliaa Mohammed <alia_20230357@fci.helwan.edu.eg>
2025-09-06 14:05:49 +0300

    Adjust margin in TodayPage layout

--- src/pages/TodayPage.jsx
+++ src/pages/TodayPage.jsx
@@ -5,7 +5,7 @@ import TodaySection from "../components/TodayPage/TodaySection";
 const TodayPage = () => {
   return (
     <div className="min-h-screen flex bg-white">
-      <div className="flex flex-col lg:flex-row p-6 gap-6 bg-white w-full max-w-6xl mx-auto">
+      <div className="flex flex-col lg:flex-row p-6 gap-6 bg-white w-full ml-0 lg:ml-6">
         <Sidebar
           title="Welcome to ToDo Py"
           description="Your ultimate task management solution."
